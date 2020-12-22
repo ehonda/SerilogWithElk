46cb1b6936ef7ba39227604f34e6a4d1a19925ed
Dennis Renz <dennis@r-renz.de>
2020-12-23 00:51:24 +0100

    Delete WeatherForecast stuff

---
+++ SerilogWithElk/Controllers/ElkController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SerilogWithElk.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ElkController : ControllerBase
+    {
+        public ElkController()
+        {
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+        }
+    }
+}
