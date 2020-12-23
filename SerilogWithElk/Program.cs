6f9d69015654461f151da2c36082b0f19b3ad321
Dennis Renz <dennis@r-renz.de>
2020-12-23 01:00:21 +0100

    Configure Serilog for Program

--- SerilogWithElk/Program.cs
+++ SerilogWithElk/Program.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+using System;
 
 namespace SerilogWithElk
 {
@@ -13,11 +11,45 @@ namespace SerilogWithElk
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+            var outputTemplate =
+                "[{Timestamp:HH:mm:ss} {Level:u3}] " +
+                "{Message:lj}{NewLine}{Exception}";
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(
+                    outputTemplate: outputTemplate)
+                .WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                    {
+                        AutoRegisterTemplate = true,
+                        BufferBaseFilename = ".logs/elasticbuffer",
+                        IndexFormat = "serilog-elk-{0:yyyy.MM.dd}"
+                    })
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                Log.Fatal(exception, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
